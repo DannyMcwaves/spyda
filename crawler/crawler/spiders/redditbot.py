e493bf3828212f54216db828e3b3722a80d9ead1
dannymcwaves <dannymcwaves96@gmail.com>
2018-01-12 12:49:21 +0000

    main reddit bot that scrapes reddit site for information

---
+++ crawler/crawler/spiders/redditbot.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RedditbotSpider(scrapy.Spider):
+    name = 'redditbot'
+    allowed_domains = ['www.reddit.com/r/gameofthrones/']
+    start_urls = ['http://www.reddit.com/r/gameofthrones//']
+
+    def parse(self, response):
+        print(response)
+        title = response.css('title::text').extract_first()
+        div = response.css('p::text').extract()
+        return {
+            'title': title,
+            'div': div
+        }
